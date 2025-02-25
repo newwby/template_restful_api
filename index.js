bd87b5ae4e6d6f877be5d852788dac611cd1befe
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-02-25 21:44:02 +0000

    adds bodyParser req & modifies default route

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
 app.listen(port, () => {
