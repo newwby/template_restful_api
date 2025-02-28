3ad13a6e14994e2b79e9619d59ffb7dc5d6c51d6
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-02-28 15:11:54 +0000

    restores getUserbyID route

--- queries.js
+++ queries.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   port: 5432,
 })
 
+
 const getAllUsers = async () => {
   return new Promise((resolve, reject) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
@@ -30,6 +31,26 @@ const getAllUsers = async () => {
 }
 
 
+async function getUser(arg_id) {
+  return new Promise((resolve, reject) => {
+    pool.query(`SELECT * FROM users WHERE id = ${arg_id}`, (error, results) => {
+      if (error) {
+        console.error(`getUser error: ${error}`)
+        error.message = `Failed to get user ${arg_id}: ${error.message}`
+        return reject(error)
+      }
+      else if (results.rows.length === 0) {
+        return reject (new Error(`getUser - user ${arg_id} not found.`))
+      }
+      else {
+        resolve(results.rows)
+      }
+    })
+  })
+}
+
+
 module.exports = {
   getAllUsers,
+  getUser,
 }
--- routes.js
+++ routes.js
@@ -4,60 +4,32 @@ const queries = require('./queries')
 const getUsers = async (request, response) => {
   try {
     const users = await queries.getAllUsers()
-    response.status(200).json({ "success": true, "data": users })
+    response.status(200).json({"success": true, "data": users })
   }
   catch (error) {
     response.status(500).json({"success": false, "error": error.message, "details": error})
   }
 }
 
-/*
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
-  let output = {
-    "status": null,
-  }
 
-  // debugger
-  if (typeof(id) !== "number") {
-    output.status = 400
+const getUserById = async (request, response) => {
+  const request_id = parseInt(request.params.id)
+  if (typeof(request_id) !== "number") {
+    response.status(400).json({"success": false, "error": `getUserById error, invalid ID: ${request_id}`})
   }
   else {
-    console.log(0)
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-      console.log(1)
-      if (error) {
-        console.log(2)
-        output.status = 500
-        output.error = error
-      }
-      else {
-        console.log(3)
-        let is_user_found = (results.rows.length !== 0)
-        if (is_user_found) {
-          console.log(4)
-          output.status = 200
-          output.data = results.rows
-        }
-        else {
-          console.log(5)
-          output.status = 404
-        }
-      }
-    })
-  }
-  
-  // send response, handling if there's a problem in the error handling
-  if (output.status === null) {
-    console.log("ERROR IN ERROR HANDLING")
-    response.status(0).json(output)
-  }
-  else {
-    response.status(output.status).json(output)
+    try {
+      const user = await queries.getUser(request_id)
+      response.status(200).json({"success": true, "data": user})
+    }
+    catch (error) {
+      response.status(500).json({"success": false, "error": error.message, "details": error})
+    }
   }
 }
-*/
+
 
 module.exports = {
     getUsers,
+    getUserById,
 }
