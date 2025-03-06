2507209410923bc3c570b4778dca42ef679b3759
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-03-06 23:01:23 +0000

    fixes error typo in middleware validateUserID

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -2,7 +2,7 @@
 const validateUserID = async (request, response, next) => {
   const request_id = parseInt(request.params.id)
   if (isNaN(request_id)) {
-    response.status(400).json({"success": false, "error": `getUserById error, ${request.params.id} is invalid ID: ${request_id}`})
+    response.status(400).json({"success": false, "error": `validateUserID error, ${request.params.id} is invalid ID: ${request_id}`})
   }
   else {
     request.request_id = request_id
